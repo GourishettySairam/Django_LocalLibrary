a34dacbf5aa74848b229e387bb03126ba074dd6a
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-01-24 15:34:54 +0530

    custom displays of models in admin panel

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 import uuid  # Required for unique book instances
 from datetime import date
 from django.contrib.auth.models import User  # Required to assign User as a borrower
-
+from django.contrib import admin
 
 # Create your models here.
 
@@ -36,9 +36,15 @@ class Book(models.Model):
         """String for representing the Model object."""
         return self.title
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
     def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+            """Returns the url to access a detail record for this book."""
+            return reverse('book-detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -96,3 +102,35 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
